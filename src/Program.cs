a47e486c900e973fc9208c891bce4053c6892186
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-06-14 18:14:30 +0000

    14 de jun. de 2024, 15:14

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.OData.Client;
+using System.Linq;
+
+namespace NavIntegrationExample
+{
+    public class NavContext : DataServiceContext
+    {
+        public NavContext(Uri serviceRoot) : base(serviceRoot) { }
+
+        public DataServiceQuery<YourEntity> YourEntities
+        {
+            get { return base.CreateQuery<YourEntity>("YourEntities"); }
+        }
+    }
+
+    [Key("ID")]
+    public class YourEntity
+    {
+        public int ID { get; set; }
+        public string FieldName { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceRoot = new Uri("http://your-server:7048/DynamicsNAV/OData/Company('YourCompany')");
+            var context = new NavContext(serviceRoot);
+
+            var query = context.YourEntities.Where(e => e.ID == 1);
+            foreach (var entity in query)
+            {
+                Console.WriteLine(entity.FieldName);
+            }
+        }
+    }
+}
